0b5d208ac0e250c7542ec1b21df9665598650c2d
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-04-11 20:52:32 +0300

    Calculates NDVI.TIF update comments

--- data_gathering/IO.py
+++ data_gathering/IO.py
@@ -12,7 +12,7 @@ class InputBands:
         self.input_path = self.parser.get_input_path()
 
     def get_selected_bands(self) -> list:
-        """Return the list with the full band names for the selected bands from the configuration file.
+        """Return the list with the full band names, as strings for the selected bands from the configuration file.
         If the file does not exist, it will not be added to the list."""
         band_names = []
         band_numbers = self.parser.get_yes_bands()
@@ -21,29 +21,17 @@ class InputBands:
         for file in os.listdir(str(self.input_path)):
             for number in band_numbers:
                 if file.endswith(strings.default_band_endwith(number)):
-                    band_names.append(file)
+                    band_names.append(str(file))
 
         return band_names
 
-    def get_attributes(self) -> dict:
-        """For now, nothing."""
-
-        attributes = {}
-        band_paths = self.get_bands_fullpath()
-
-        for band in band_paths:
-            pass
-            # TODO extract band attributes from each band with gdal
-
-        return attributes
-
     def get_bands_fullpath(self) -> list:
-        """Returns a list with full paths to the geotiff images as Paths."""
+        """Returns a list with full paths to the geotiff images as strings."""
         band_paths = []
         bands = self.get_selected_bands()
 
         # combine thg input path with the name of the geotiff band
         for band in bands:
-            band_paths.append(self.input_path.joinpath(band))
+            band_paths.append(str(self.input_path.joinpath(band)))
 
         return band_paths
