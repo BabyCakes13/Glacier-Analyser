3b2af992ab363389518e810559918484a6f6a609
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-04-11 21:42:19 +0300

    Refactored raster.

--- data_processing/raster_setup.py
+++ data_processing/raster_setup.py
@@ -1,32 +1,28 @@
-"""Module which contains the class which creates the Normalised Difference Snow Index (NDSI) file."""
-import numpy
-from osgeo import gdal
-import pathlib
+from osgeo import gdal, gdalnumeric, gdalconst, gdal_array, osr
 from util import strings
-from data_gathering import configuration, IO
+from data_gathering import configuration
+import numpy
 
-OUT_TIFF_INT16 = "NDSI_INT16.tif"
-OUT_TIFF_FLOAT32 = "NDSI_FLOAT32.tif"
-VALID_DATA_TYPES = [gdal.GDT_UInt16, gdal.GDT_Float32]
+COMPOSITE_NAME = "composite.vrt"
+NDSI_BAND_1 = "_B3.TIF"
+NDSI_BAND_2 = "_B6.TIF"
+OUT_TIFF_INT16 = "NDVI_INT16.tif"
+OUT_TIFF_FLOAT32 = "NDVI_FLOAT32.tif"
 
 
-class NDSI:
-    """Class which handles the creation of the Normalised Difference Snow Index (NDSI) file."""
+class RasterHandler:
 
-    def __init__(self):
-        """Set the needed parameters for calculating the NDSI file."""
-        self.IO_parser = IO.InputBands()
+    def __init__(self, bands):
 
+        gdal.UseExceptions()
+
+        self.all_bands = bands
         self.tiffs, self.NDSI_bands = self.setup_NDSI_bands()
-        self.rows, self.columns, self.geotransform = self.setup_NDSI_characteristics()
+        self.rows, self.columns, self.geotransform = self.setup_NDSI_charachteristics()
 
-        self.create_NDSI(OUT_TIFF_INT16, gdal.GDT_Float32)
+        self.create_NDSI('output1.TIF')
 
-    def create_NDSI(self, output_name, data_type) -> pathlib.Path:
-        """Returns the full path to the outputted NDSI image."""
-        if data_type not in VALID_DATA_TYPES:
-            print(strings.error_messages()[0])
-            exit(-1)
+    def create_NDSI(self, output_name, data_type=gdal.GDT_Float32):
 
         gdal.UseExceptions()
 
@@ -37,55 +33,67 @@ class NDSI:
             numpy_arrays.insert(count, band.ReadAsArray(0, 0, self.columns, self.rows))
             numpy_arrays_as32.insert(count, numpy_arrays[count].astype(numpy.float32))
 
-        # NDsI formula
+        # NDVI formula
         numpy.seterr(divide='ignore', invalid='ignore')
+
         numerator = numpy.subtract(numpy_arrays_as32[0], numpy_arrays_as32[1])
         denominator = numpy.add(numpy_arrays_as32[0], numpy_arrays_as32[1])
         result = numpy.divide(numerator, denominator)
 
-        # transform the -0's resulted from the 0 division in -99
-        # transform the -0's resulted from the 0 division in -99
         result[result == -0] = -99
 
         geotiff = gdal.GetDriverByName('GTiff')
-        config_parser = configuration.ReadConfig()
-        output_fullpath = str(config_parser.get_output_path().joinpath(output_name))
+        parser = configuration.ReadConfig()
+        output_fullpath = parser.get_output_path().joinpath(output_name)
 
-        # In case the output is int6 ([-1,1]), convert values to [0,255] to create a int6 geotiff with one band
         if data_type == gdal.GDT_UInt16:
-            result = numpy.multiply((result + 1), (2 ** 7 - 1))
-            data_type = gdal.GDT_Byte
+            ndvi_int8 = numpy.multiply((result + 1), (2 ** 7 - 1))
+            output = geotiff.Create(output_fullpath, self.columns, self.rows, 1, gdal.GDT_Byte)
+            output_band = output.GetRasterBand(1)
+            output_band.SetNoDataValue(-99)
+            output_band.WriteArray(ndvi_int8)
+        elif data_type == gdal.GDT_Float32:
+            output = geotiff.Create(str(output_fullpath), self.columns, self.rows, 1, gdal.GDT_Float32)
+            output_band = output.GetRasterBand(1)
+            output_band.SetNoDataValue(-99)
+            output_band.WriteArray(result)
+        else:
+            raise ValueError('Invalid output data type.  Valid types are gdal.UInt16 or gdal.Float32.')
 
-        # setup the output image
-        output_tiff = geotiff.Create(output_fullpath, self.columns, self.rows, 1, data_type)
-        output_band = output_tiff.GetRasterBand(1)
-        output_band.SetNoDataValue(-99)
-        output_band.WriteArray(result)
+            # Set the geographic transformation as the input.
+        output.SetGeoTransform(self.geotransform)
 
-        return output_fullpath
+        return None
 
     def setup_NDSI_bands(self) -> tuple:
         """Returns two lists formed of the opened NDSI tiffs (gdal.Dataset) and NDSI bands (gdal.Band)."""
-        gdal.UseExceptions()
-
-        # get the NDSI bands full paths for opening
-        NDSI_band_paths = self.IO_parser.get_NDSI_band_paths()
-        NDSI_tiffs = []
+        NDSI_band_paths = self.get_NDSI_band_paths()
+        tiffs = []
         NDSI_bands = []
 
         # populate the tiffs list with the opened tiff images and extract the bands from the tiffs into the bands list
         for count, band in enumerate(NDSI_band_paths, start=0):
-            NDSI_tiffs.insert(count, gdal.Open(band))
-            NDSI_bands.insert(count, NDSI_tiffs[count].GetRasterBand(1))
+            tiffs.insert(count, gdal.Open(band))
+            NDSI_bands.insert(count, tiffs[count].GetRasterBand(1))
 
-        return NDSI_tiffs, NDSI_bands
+        return tiffs, NDSI_bands
 
-    def setup_NDSI_characteristics(self) -> tuple:
+    def setup_NDSI_charachteristics(self) -> tuple:
         """Returns the number of rows, number of columns and the geographic data for transforming the output image."""
-        gdal.UseExceptions()
-
         rows = self.tiffs[0].RasterYSize
         columns = self.tiffs[0].RasterXSize
         geotransform = self.tiffs[0].GetGeoTransform()
 
         return rows, columns, geotransform
+
+    def get_NDSI_band_paths(self) -> list:
+        """Returns the full paths to the needed bands for calculating the NDSI."""
+        NDSI_band_paths = []
+        for band in self.all_bands:
+            if NDSI_BAND_1 in band or NDSI_BAND_2 in band:
+                NDSI_band_paths.append(band)
+
+        return NDSI_band_paths
+
+
+
