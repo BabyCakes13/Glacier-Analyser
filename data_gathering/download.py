8b0e8979aa2a70b829048daaade600d10212cfb9
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-05-08 22:16:49 +0300

    add multi-process downloading

--- data_gathering/download.py
+++ data_gathering/download.py
@@ -6,19 +6,17 @@ import subprocess
 
 class CSVHandler:
 
-    def __init__(self, start_row=0, end_row=definitions.MAX_ROW):
+    def __init__(self, input_csv, output_dir, start_row=0, end_row=definitions.MAX_ROW):
 
-        self.glacier_csv = os.path.join(definitions.FILES_DIR, definitions.GLACIER_COORDINATES_FILENAME + '.csv')
-        endwith = '_' + str(start_row) + '_' + str(end_row) + '.txt'
-        self.glacier_txt = os.path.join(definitions.FILES_DIR,
-                                        definitions.GLACIER_COORDINATES_FILENAME + endwith)
+        self.glacier_csv = input_csv
+        self.output_dir = output_dir
 
         self.start_row = start_row
         self.end_row = end_row
 
-        self.max_threads = 100
+        self.max_threads = 10
 
-        self.txt_file = open(self.glacier_txt, "w")
+        print ( "Input: " + str(input_csv) + " from : ", start_row, " to: ", end_row)
 
     def open_csv(self):
         with open(self.glacier_csv, 'r', newline='', encoding='ISO-8859-1') as csv_file:
@@ -29,33 +27,45 @@ class CSVHandler:
         """Method which parses the csv rows and writes the coordinates to the text file."""
         # first row is the row name
 
-        simultaneous_processes = []
+        pQuery = []
 
         for row in csv_reader: # csv_without_first_row:
             coordinates = ( row['lon'], row['lat'] )
-            self.txt_file.write(str(coordinates) + '\n')
 
             bbox_size = 0.0000001
 
-            json_query_filename = os.path.join(definitions.FILES_DIR,
-                                               "query_" + row['lon'] + "_" + row['lat'] + ".json")
+            id = row['wgi_glacier_id'] # + "_" + row['lon'] + "_" + row['lat']
 
-            arglist = ["sat-search", "search", "--bbox",
-                       str(float(row['lon']) - bbox_size),
-                       str(float(row['lat']) - bbox_size),
-                       str(float(row['lon']) + bbox_size),
-                       str(float(row['lat']) + bbox_size),
-                       "--save", json_query_filename]
+            dirname = os.path.join(self.output_dir, id)
+            json_query_filename = os.path.join(dirname, "query.json")
+            os.mkdir(dirname)
 
-            if len(simultaneous_processes) >= self.max_threads:
-                simultaneous_processes.pop().wait()
+            queryArglist = ["sat-search", "search", "--bbox",
+                            str(float(row['lon']) - bbox_size),
+                            str(float(row['lat']) - bbox_size),
+                            str(float(row['lon']) + bbox_size),
+                            str(float(row['lat']) + bbox_size),
+                            "--save", json_query_filename]
 
-            print( arglist )
-            sp = subprocess.Popen( arglist )
-            simultaneous_processes.append(sp)
+            downloadArglist = ["sat-search", "load", json_query_filename,
+                            "--download", "B3", "B6",
+                            "--datadir", dirname]
 
-            for sp in simultaneous_processes:
+            if len(pQuery) >= self.max_threads:
+                filename, sp = pQuery.pop()
+                sp.wait()
+                print("Query done: ", filename)
+
+            print( queryArglist )
+            sp = subprocess.call( queryArglist )
+
+            print( downloadArglist )
+            sp = subprocess.Popen( downloadArglist )
+            pQuery.append((json_query_filename, sp))
+
+            for filename, sp in pQuery:
                 if sp.poll() == 0:
-                    simultaneous_processes.remove(sp)
+                    pQuery.remove((filename, sp))
+                    print("Query done: ", filename)
 
         self.txt_file.close()
--- main.py
+++ main.py
@@ -2,12 +2,44 @@
 
 """Main caller of the application."""
 from data_gathering import download
+import argparse
+import os
+import definitions
 
-# works
-# csvhandler = download.CSVHandler(0, 20)
-# csvhandler.open_csv()
+# sub-command functions
+def fDownload(args):
+    if args.input:
+        inputCsv = args.input
+    else:
+        inputCsv = os.path.join(definitions.FILES_DIR, definitions.GLACIER_COORDINATES_FILENAME + '.csv')
 
-# BUG crashes at line 839, position 2201
-# (UnicodeDecodeError: 'utf-8' codec can't decode byte 0xf1 in position 2201: invalid continuation byte)
-csvhandler = download.CSVHandler()
-csvhandler.open_csv()
+    if args.output:
+        outputDir = args.output
+    else:
+        outputDir = os.path.join(definitions.FILES_DIR)
+
+    csvhandler = download.CSVHandler(inputCsv, outputDir, args.first, args.last)
+    csvhandler.open_csv()
+
+def process(args):
+    print ('((%s))' % args.z)
+
+# create the top-level parser
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+# create the parser for the "download" command
+parser_download = subparsers.add_parser('download')
+parser_download.add_argument('--input')
+parser_download.add_argument('--output')
+parser_download.add_argument('--first', type=int, default=0)
+parser_download.add_argument('--last', type=int, default=99999999)
+parser_download.set_defaults(func=fDownload)
+
+# create the parser for the "process" command
+parser_process = subparsers.add_parser('process')
+parser_process.add_argument('z')
+parser_process.set_defaults(func=process)
+
+args = parser.parse_args()
+args.func(args)
