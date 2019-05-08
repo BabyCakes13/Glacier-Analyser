f666995169f1fbcd13f789b424d1f70836c53d06
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-05-08 19:07:11 +0300

    simplify

--- data_gathering/download.py
+++ data_gathering/download.py
@@ -6,7 +6,7 @@ import itertools
 
 class CSVHandler:
 
-    def __init__(self, start_row=-0, end_row=definitions.MAX_ROW):
+    def __init__(self, start_row=0, end_row=definitions.MAX_ROW):
 
         self.glacier_csv = os.path.join(definitions.FILES_DIR, definitions.GLACIER_COORDINATES_FILENAME + '.csv')
         endwith = '_' + str(start_row) + '_' + str(end_row) + '.txt'
@@ -21,7 +21,7 @@ class CSVHandler:
     def open_csv(self):
 
         with open(self.glacier_csv, 'r', newline='', encoding='ISO-8859-1') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
+            csv_reader = csv.DictReader(csv_file)
             self.parse_rows(csv_reader)
 
     # TODO remove \n from end of file.
@@ -29,22 +29,9 @@ class CSVHandler:
     def parse_rows(self, csv_reader):
         """Method which parses the csv rows and writes the coordinates to the text file."""
         # first row is the row name
-        csv_without_first_row = itertools.islice(enumerate(csv_reader), 1, None)
 
-        for current_row, row in csv_without_first_row:
-            if current_row == self.end_row:
-                break
-            if self.start_row <= current_row <= self.end_row:
-                coordinates = extract_coordinates(row)
-                self.txt_file.write(str(coordinates) + '\n')
+        for row in csv_reader: # csv_without_first_row:
+            coordinates = ( row['lat'], row['lon'] )
+            self.txt_file.write(str(coordinates) + '\n')
 
         self.txt_file.close()
-
-
-def extract_coordinates(row) -> tuple:
-    """Method which extracts the coordinates from the given csv row."""
-    coordinates = (row[definitions.CSV_LATITUDE_POSITION],
-                   row[definitions.CSV_LONGITUDE_POSITION])
-
-    return coordinates
-
