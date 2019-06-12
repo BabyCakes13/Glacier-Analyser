576ff5eba014e670c6f890a74fc25c9a61a1b812
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-06-13 00:40:13 +0300

    Add simple profile data (INCOMPLETE)
    complete me ?!?!?!??!

--- main.py
+++ main.py
@@ -3,4 +3,15 @@
 """Main caller of the application."""
 from data_gathering import arguments
 
+import cProfile, pstats, io
+pr = cProfile.Profile()
+pr.enable()
+
 arguments.activate_arguments()
+
+pr.disable()
+s = io.StringIO()
+sortby = 'cumulative'
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print( s.getvalue())
