28819388b92fc18e1a78012ec22490edd746a918
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-06-25 20:04:49 +0300

    Refactored download.py.

--- data_gathering/download.py
+++ data_gathering/download.py
@@ -1,20 +1,29 @@
 import csv
-from colors import *
 import os
 import shutil
 import subprocess
+
+from colors import *
+
 import definitions
 from data_processing import multiprocess as mh
 
 
 class Downloader:
-    """Class which handles searching and downloading landsat files."""
+    """
+    Class which handles bulk searching and downloading of Landsat 8 scenes, multiprocessed.
+    """
 
-    def __init__(self, input_csv=definitions.GLACIER_DATASET_PATH,
+    def __init__(self, input_csv_path=definitions.GLACIER_DATASET_PATH,
                  download_dir=definitions.DEFAULT_DOWNLOAD_DIR,
                  max_processes=definitions.MAX_PROCESSES):
-        """Initialises variables needed for the processes."""
-        self.glacier_csv = input_csv
+        """
+        Initializes the variables needed for download processing.
+        :param input_csv_path: The csv which contains information about the glaciers to be downloaded.
+        :param download_dir: The path to the download directory in which the downloaded paths will be stored.
+        :param max_processes: The number of maximum processes which can be turned on for the multiprocessing.
+        """
+        self.glacier_csv_path = input_csv_path
         self.download_dir = download_dir
         self.max_processes = max_processes
 
@@ -23,16 +32,27 @@ class Downloader:
 
         self.INTERRUPT_SIGNAL = False
 
-        print(blue("Download csv file: ") + str(input_csv))
+        print(blue("Download csv file: ") + str(input_csv_path))
 
-    def start(self):
+    def start(self) -> None:
+        """
+        Method which reads the glacier csv files and parses its rows in order to begin the processing of each one.
+        :return: None
+        """
         """Opens the csv file and starts the searching and downloading process."""
-        with open(self.glacier_csv, 'r', newline='', encoding='ISO-8859-1') as csv_file:
+        with open(self.glacier_csv_path, 'r', newline='', encoding='ISO-8859-1') as csv_file:
             csv_reader = csv.DictReader(csv_file)
             self.parse_rows(csv_reader)
 
     @staticmethod
-    def create_search_arglist(row, json_query_filename):
+    def create_search_arglist(row, json_query_filename) -> list:
+        """
+        Method which creates the search task from the command line which will be transferred to the multiprocessing
+        handler. It calculates the bounding box of coordinates.
+        :param row: The current row which is being processed from the csv.
+        :param json_query_filename: The name of the json query filename which will contain the results of the search.
+        :return: list
+        """
         """Creates the sat-search argument list."""
         search_arglist = ["sat-search", "search", "--bbox",
                           str(float(row['lon']) - definitions.BBOX_SIZE),
@@ -45,21 +65,41 @@ class Downloader:
         return search_arglist
 
     @staticmethod
-    def create_download_arglist(json_query_filename, dirname):
-        """Creates the sat-load argument list."""
+    def create_download_arglist(json_query_filename, download_dir) -> list:
+        """
+        Method which creates the download task from the command line which will be transferred to the multiprocessing
+        handler. It specifies that only the green band, swir1 band, MTL file and the thumbnail should be downloaded.
+        :param json_query_filename: The name fo the json query resulted from searching.
+        :param download_dir: The directory in which the files will be downloaded.
+        :return: list
+        """
         download_arglist = ["sat-search", "load", json_query_filename,
                             "--download", "B3", "B6", "MTL", "thumbnail",
-                            "--datadir", dirname]
+                            "--datadir", download_dir]
 
         return download_arglist
 
     @staticmethod
-    def process_handler(task_name, return_code):
+    def process_handler(task_name, return_code) -> None:
+        """
+        The handler function which gets the return code from the subprocess and handles the result by outputting the
+        message.
+        :param task_name: The name of the task which finished.
+        :param return_code: The return code of the process which finished.
+        :return: None
+        """
         return_codes = definitions.RETURN_CODES
         return_code = return_codes[return_code]
         print(return_code, " ", task_name)
 
-    def parse_rows(self, csv_reader):
+    def parse_rows(self, csv_reader) -> None:
+        """
+        Method which parses the rows representing glaciers and their information from the glacier csv file. It handles
+        dictionary creation as: for each glacier, create an directory which has the glacier name and its coordinates in
+        the name.
+        :param csv_reader: The csv handler.
+        :return: None
+        """
         """Parses and processes each row in the CSV file."""
         for row in csv_reader:
 
@@ -78,7 +118,7 @@ class Downloader:
                 search_task = self.create_search_arglist(row, json_query_filename)
                 print(yellow(search_task))
                 # sync, will wait to finish
-                search = subprocess.call(search_task)
+                subprocess.call(search_task)
 
                 # async, will not wait to finish
                 download_task = self.create_download_arglist(json_query_filename, directory_name)
@@ -92,5 +132,3 @@ class Downloader:
                 self.mh.kill_all_processes()
                 self.mh.wait_all_process_done()
                 break
-
-
--- data_gathering/scene_metadata.py
+++ data_gathering/scene_metadata.py
@@ -3,49 +3,73 @@ from util import strings
 
 
 class SceneMetadata:
-    """Class which deals with the scene MTL metadata file."""
+    """
+    Class which handles the extraction of information from the metadata file found in the download directory of each
+    scene. Based on working with .MTL file.
+    """
 
     def __init__(self, metadata_path):
         """Sets up the metadata handler."""
         self.metadata = metadata_path
 
     def get_scene_set_attributes(self) -> dict:
-        """Returns a dictionary with the set landsat scene attributes from the MTL file."""
+        """
+        Returns a dictionary with the set landsat scene attributes from the MTL file.
+        :return: dict
+        """
         attributes = strings.get_scene_unset_attributes()
 
         with open(self.metadata, "r") as file:
             for line in file:
-                attributes = set_dictionary(attributes, line, ' = ')
+                attributes = self.set_dictionary(attributes, line, ' = ')
 
         return attributes
 
     def get_scene_set_coordinates(self) -> dict:
-        """Returns a dictionary with the set landsat scene coordinates from the MTL file."""
+        """
+        Returns a dictionary with the set landsat scene coordinates from the MTL file.
+        :return: dict
+        """
         coordinates = strings.get_scene_unset_coordinates()
 
         with open(self.metadata, "r") as file:
             for line in file:
-                coordinates = set_dictionary(coordinates, line, ' = ')
+                coordinates = self.set_dictionary(coordinates, line, ' = ')
 
         return coordinates
 
+    @staticmethod
+    def set_dictionary(dictionary, line, splitter) -> dict:
+        """
+        Sets the values of the dictionary given as parameter by splitting the given line
+        on the given splitter.
+        :param dictionary: The dictionary to be split.
+        :param line: The line containing key, value pairs.
+        :param splitter: The splitter character used for splitting the lines and separate the key values.
+        :return: dict
+        """
+        for key, value in dictionary.items():
+            if key in line:
+                (set_key, set_val) = line.rstrip().split(splitter)
+                dictionary[key] = set_val
 
-def set_dictionary(dictionary, line, splitter) -> dict:
-    """Sets the values of the dictionary given as parameter by splitting the given line
-    on the given splitter."""
-    for key, value in dictionary.items():
-        if key in line:
-            (set_key, set_val) = line.rstrip().split(splitter)
-            dictionary[key] = set_val
-
-    return dictionary
+        return dictionary
 
 
 class SceneMetadataError(Exception):
-    """Raise for the case when the metadata file doesn't exist in the input directory."""
-
+    """
+    Class which raises an exception when there is no metadata file found.
+    """
     def __init__(self, message):
+        """
+        Initializes a message to return when the exception is raised.
+        :param message: The error message when the file is not found
+        """
         self.message = message
 
-    def __str__(self):
+    def __str__(self) -> repr:
+        """
+        Returns the repr string containing the error message..
+        :return: repr
+        """
         return repr(self.message)
